12904ee8f24fc57afda10c85163bda26caf7c5fa
Ágatha Almeida <142939923+AgathaDevBr@users.noreply.github.com>
2025-09-21 20:38:17 -0300

    AGORA FOI

---
+++ DevFreela.Core/Exceptions/ProjectAlreadyStarttedException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Exceptions
+{
+    public class ProjectAlreadyStarttedException : Exception
+    {
+        public ProjectAlreadyStarttedException() : base("Project already started and cannot be started again.") { }
+    }
+}
