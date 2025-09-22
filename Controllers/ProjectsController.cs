ddec90ff3d9094583fcf0191ed7298311d26709e
AgathaSantos <agathawebdev@gmail.com>
2025-09-21 22:50:00 -0300

    #f19

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace DevFreela.API.Controllers
 {
@@ -7,10 +6,34 @@ namespace DevFreela.API.Controllers
     [ApiController]
     public class ProjectsController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Get()
-        {
 
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        {
+            try
+            {
+                if (createProject.Title.Length > 50)
+                {
+                    return BadRequest("O título deve ter no máximo 50 caracteres.");
+                }
+
+                return CreatedAtAction(nameof(GetById), new { id = 1 }, null);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
             return Ok();
         }
     }
