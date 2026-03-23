c80cdc75256e73783b5c90817eacb544aedd6445
AgathaSantos <agathawebdev@gmail.com>
2026-03-23 00:36:54 -0300

    .

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-using DevFreela.Infrastructure.Persistence;
+﻿using DevFreela.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using System.Configuration;
@@ -10,7 +11,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSingleton<DevFreelaDbContext>();
+
+
+// ✅ Substitua AddScoped pelo AddDbContext com a connection string
+builder.Services.AddDbContext<DevFreelaDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DevFreelaAPIContext")));
+
+// ✅ Mova o AddSwaggerGen para ANTES do builder.Build()
+builder.Services.AddSwaggerGen(c =>
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "DevFreela.API", Version = "v1" }));
+
 var app = builder.Build();
 
 builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo {  Title = "DevFreela.API", Version = "v1"}));
