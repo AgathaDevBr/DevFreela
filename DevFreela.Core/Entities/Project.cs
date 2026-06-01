7b8e18d6a3f3546853d67109e91f70dda41da74c
AgathaSantos <agathawebdev@gmail.com>
2026-05-31 21:49:42 -0300

    Entitie project

--- DevFreela.Core/Entities/Project.cs
+++ DevFreela.Core/Entities/Project.cs
@@ -35,8 +35,10 @@ namespace DevFreela.Core.Entities
         public List<ProjectComment> Comments { get; private set; }
         public void Cancel()
         {
-            if (Status == ProjectStatusEnum.InProgress || Status == ProjectStatusEnum.InProgress){
-              Status = ProjectStatusEnum.Cancelled;   
+            if (Status == ProjectStatusEnum.Created ||
+                Status == ProjectStatusEnum.InProgress)
+            {
+                Status = ProjectStatusEnum.Cancelled;
             }
         }
 
